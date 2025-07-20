3b6b0d2be751cec00b5000f9f02cfe779d18b002
rafaellrossi <rafaelrossij@gmail.com>
2025-07-19 23:23:21 -0300

    Adiciona novos arquivos ao .gitignore e importa módulos random e json no app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import requests
 import decimal
+import random
+import json
 import os
 from dotenv import load_dotenv
 
@@ -20,47 +22,43 @@ while ver == True:
     else:
         tokens.append(token)
         i += 1
-        
-x = 0 
-     
+
+x = 0
+
 for token in tokens:
     x += 1
-    
-    msg = f"ID: {x} | "
-    
+
     headers = {
-        "User-Agent": "Dart/3.4 (dart:io)",
+        "User-Agent": "Dart/3.3 (dart:io)",
         "Accept-Encoding": "gzip",
-        "Content-Type": "application/json",
-        "app_version": "4.3.1",
-        "authorization": f"Bearer {token}",
+        "x-access-token": token,
     }
 
-    getHash = requests.get(f"{url}/machine:hasFree", headers=headers)
+    response = requests.get(f"{url}/mining-contracts", headers=headers)
 
-    if getHash.status_code == 200:
-        has = getHash.json()
-        if has["data"]["has"]:
-            claimHash = requests.post(f"{url}/machine:receiveFree", headers=headers)
-            msg += f"Claim Hash: {claimHash.json()['data']['miningRatePerCore']} | "
+    if response.status_code == 200:
+
+        # 24 horas
+        response = requests.post(f"{url}/daily-reward", headers=headers)
+
+        # 8 horas
+        response = requests.post(
+            f"{url}/free-mining-contract", headers=headers)
+
+        # Ver Ads 24 horas 4.02 Gh/s
+        payload = {
+            "ecpm_tier": "FIVE",
+        }
+        response = requests.post(
+            f"{url}/rewarded-mining-contract", data=payload, headers=headers)
+
+        # Sacar
+        response = requests.get(url, headers=headers)
+        balance = response.json()["btc_balance_in_satoshi_nanos"]
+        if balance >= 10000000000:
+            print(f"ID: {x} | Sacando: {balance}")
         else:
-            msg += "Hash NÃO disponível. | "
-
-        balance = requests.get(f"{url}/machine:income", headers=headers).json()
-        b = balance["data"]["balance"]
-        minimo = requests.get(f"{url}/config:get?appId=3", headers=headers).json()
-        m = minimo["data"]["withdrawal_limit"]["lightning"].split("-")[0]
-
-        if b > m:
-            d = decimal.Decimal(b)
-            btc = float(d.quantize(decimal.Decimal("0.00000001"), rounding=decimal.ROUND_DOWN))
-            urlSacar = f"{url}/withdraw:apply?chain=speed&walletType=speedaddr&asset=BTC&amount={btc}&wallet={wallet}"
-            sacar = requests.post(urlSacar, headers=headers)
-            msg += f"Saque recebido: {sacar.json()['data']['receivedAmount']} | Saldo: {sacar.json()['data']['balance']}"
-        else:
-            msg += f"Saldo insuficiente para saque. | Saldo: {b} | Mínimo: {m}"
-        
-        print(msg)
+            print(f"ID: {x} | Saldo insuficiente para saque: {balance}")
 
     else:
         print("Erro ao acessar a API:")
